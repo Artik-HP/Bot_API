6ef9e8a34d9c3bb9712c3d2fa5f72b9a5429a0ae
Artik-HP <artik95android@gmail.com>
2026-05-23 19:34:15 +0300

    add health endpoint and docs

--- index.js
+++ index.js
@@ -150,3 +150,11 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`)
   next()
 })
+
+app.get('/health', (req, res) => {
+  res.json({
+    ok: true,
+    status: 'healthy',
+    uptime: process.uptime()
+  })
+})
